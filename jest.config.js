3d131fe6f4a681f7f917fd5ce3daa5c9aa9fd00e
Jon Ambas <jon@jonambas.com>
2022-03-12 20:27:52 -0500

    Try again

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '@delver/logger(.*)': '<rootDir>/packages/logger/src',
-    '@delver/react(.*)': '<rootDir>/packages/react/src',
+    '@delver/logger': '<rootDir>/packages/logger/src',
     chalk: 'chalk/source/index.js',
     '#ansi-styles': 'chalk/source/vendor/ansi-styles/index.js',
     '#supports-color': 'chalk/source/vendor/supports-color/index.js'
