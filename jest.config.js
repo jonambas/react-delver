5e8a40a30b0383c1f631cdacf29cf975b7578c3d
jonambas <jon@jonambas.com>
2022-06-25 09:38:19 -0400

    chore: remove unused jest configurations

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    chalk: 'chalk/source/index.js',
-    '#ansi-styles': 'chalk/source/vendor/ansi-styles/index.js',
-    '#supports-color': 'chalk/source/vendor/supports-color/index.js'
-  },
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](build|docs|node_modules|scripts)[/\\\\]'],
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  testPathIgnorePatterns: [
+    '<rootDir>[/\\\\](build|docs|node_modules|scripts)[/\\\\]'
+  ],
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'
+  ],
   extensionsToTreatAsEsm: ['.ts'],
   globals: {
     'ts-jest': {
